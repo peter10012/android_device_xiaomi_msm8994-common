31a6a34c7fcedcf452c11e9e5593534523795dcf
dkmos2016 <dkmos2016@126.com>
2024-02-04 17:17:14 +0800

    leo: add module init

---
+++ init/libinit_msm8994.cpp
@@ -0,0 +1,226 @@
+/*
+   Copyright (c) 2016, The CyanogenMod Project
+   Copyright (c) 2019, The LineageOS Project
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstdlib>
+#include <fstream>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
+
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "vendor_init.h"
+#include "property_service.h"
+
+#include "libinit_msm8994.h"
+
+using android::base::GetProperty;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+    "",
+};
+
+void property_override(char const prop[], char const value[], bool add)
+{
+    auto pi = (prop_info *)__system_property_find(prop);
+
+    if (pi != nullptr)
+    {
+        __system_property_update(pi, value, strlen(value));
+    }
+    else if (add)
+    {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product)
+{
+    std::string prop_name;
+
+    for (const auto &source : ro_props_default_source_order)
+    {
+        if (product)
+            prop_name = "ro.product." + source + prop;
+        else
+            prop_name = "ro." + source + "build." + prop;
+
+        property_override(prop_name.c_str(), value.c_str(), true);
+    }
+}
+
+void set_dalvik_heap_size()
+{
+    struct sysinfo sys;
+
+    char const *heapstartsize;
+    char const *heapgrowthlimit;
+    char const *heapsize;
+    char const *heapminfree;
+    char const *heapmaxfree;
+    char const *heaptargetutilization;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > 5072ull * 1024 * 1024)
+    {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    }
+    else if (sys.totalram > 3072ull * 1024 * 1024)
+    {
+        // from - phone-xxhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    }
+    else
+    {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "288m";
+        heapsize = "768m";
+        heaptargetutilization = "0.75";
+        heapminfree = "2m";
+        heapmaxfree = "8m";
+    }
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+}
+
+void set_variant_props(const variant_info_t variant)
+{
+    set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("device", variant.device, true);
+    set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("model", variant.model, true);
+
+    set_ro_build_prop("fingerprint", variant.build_fingerprint);
+    property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint.c_str());
+    property_override("ro.build.description", variant.build_description.c_str());
+}
+
+#ifdef LIBINIT_MSM8937_MODS
+/* From Magisk@native/jni/magiskhide/hide_utils.c */
+static const char *cts_prop_key[] =
+    {"ro.boot.vbmeta.device_state", "ro.boot.verifiedbootstate", "ro.boot.flash.locked",
+     "ro.boot.veritymode", "ro.boot.warranty_bit", "ro.warranty_bit",
+     "ro.debuggable", "ro.secure", "ro.build.type", "ro.build.tags",
+     "ro.vendor.boot.warranty_bit", "ro.vendor.warranty_bit",
+     "vendor.boot.vbmeta.device_state", nullptr};
+
+static const char *cts_prop_val[] =
+    {"locked", "green", "1",
+     "enforcing", "0", "0",
+     "0", "1", "user", "release-keys",
+     "0", "0",
+     "locked", nullptr};
+
+static const char *cts_late_prop_key[] =
+    {"vendor.boot.verifiedbootstate", nullptr};
+
+static const char *cts_late_prop_val[] =
+    {"green", nullptr};
+
+static void workaround_cts_properties()
+{
+    // Hide all sensitive props
+    for (int i = 0; cts_prop_key[i]; ++i)
+    {
+        property_override(cts_prop_key[i], cts_prop_val[i]);
+    }
+    for (int i = 0; cts_late_prop_key[i]; ++i)
+    {
+        property_override(cts_late_prop_key[i], cts_late_prop_val[i]);
+    }
+    // Xiaomi cross region flash
+    property_override("ro.boot.hwc", "GLOBAL");
+    property_override("ro.boot.hwcountry", "GLOBAL");
+}
+
+static const char *build_keys_props[] =
+    {
+        "ro.build.tags",
+        "ro.odm.build.tags",
+        "ro.product.build.tags",
+        "ro.system.build.tags",
+        "ro.system_ext.build.tags",
+        "ro.vendor.build.tags",
+        nullptr};
+
+static const char *sp_date_props[] =
+    {
+        //  "ro.build.version.security_patch",
+        "ro.vendor.build.security_patch",
+        nullptr};
+
+void load_mods()
+{
+    /* Workaround CTS */
+    workaround_cts_properties();
+
+    /* Custom Build Fingerprint */
+    set_ro_build_prop("fingerprint", "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys");
+    property_override("ro.bootimage.build.fingerprint", "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys");
+    property_override("ro.build.description", "raven-user 12 SD1A.210817.036 7805805 release-keys");
+
+    /* Spoof Build keys */
+    for (int i = 0; build_keys_props[i]; ++i)
+    {
+        property_override(build_keys_props[i], "release-keys");
+    }
+
+    /* Spoof Security Patch Date */
+    for (int i = 0; sp_date_props[i]; ++i)
+    {
+        property_override(sp_date_props[i], "2021-10-05");
+    }
+}
+#endif
